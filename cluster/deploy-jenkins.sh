e1cee47f4136e0bd174cd076c72accae0279d432
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-02-23 15:17:04 +0000

    (feat): helm for jenkins

--- cluster/deploy-jenkins.sh
+++ cluster/deploy-jenkins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 helm init
-helm install --name jenkins stable/jenkins
+helm install --name jenkins stable/jenkins -f /home/ec2-user/terraform-aws/cluster/values.yaml
 
 export SERVICE_IP=$(kubectl get svc --namespace default jenkins-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
 echo http://${SERVICE_IP}:8080/login

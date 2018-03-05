9afdee15ee4fee23293f095ab06f70484a93d0d6
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-03-05 12:38:08 +0000

    (chore): reduce master and node size so we don't blow AWS budget

--- cluster/create.sh
+++ cluster/create.sh
@@ -9,6 +9,6 @@ export NAME=phud.k8s.local
 export AWS_DEFAULT_PROFILE=default
 export AWS_PROFILE=default
 
-kops create cluster --name ${NAME} --zones eu-west-2a --state ${KOPS_STATE_STORE} --yes --master-size=t2.micro --node-size=t2.micro
+kops create cluster --name ${NAME} --zones eu-west-2a --state ${KOPS_STATE_STORE} --yes --master-size=t2.small --node-size=t2.small
 
 echo "To check cluster - wait a couple of minutes, then: kops validate cluster --state=${KOPS_STATE_STORE}"
